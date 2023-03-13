f44b57a13f59a385a3459f389e4e4da24f315d5d
GeraldMajoni <majonigerald@gmail.com>
2023-03-13 15:33:39 +0200

    Update 6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
-# 6-print_matrix_integer.py
-
 def print_matrix_integer(matrix=[[]]):
-    """Print a matrix of integers."""
-    for digit1 in range(0, 10):
-        for digit2 in range(digit1 + 1, 10):
-            if digit1 == 8 and digit2 == 9:
-                print("{}{}".format(digit1, digit2))
-            else:
-                print("{}{}".format(digit1, digit2), end=", ")
+    for row in matrix:
+        for col in row:
+            print("{:d}".format(col), end=" " if col != row[-1] else "")
+        print()
